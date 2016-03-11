9005081602524088ac27b2838752af1a6cd1fa77
UsherBaby <ThePacific@yeah.net>
2016-03-11 14:03:41 +0800

    add git ignore filter

--- AndroidExample/Unix/app/src/main/java/com/example/ipad/unix/MainActivity.java
+++ AndroidExample/Unix/app/src/main/java/com/example/ipad/unix/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.ipad.unix;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
@@ -12,20 +14,30 @@ import android.view.View;
 import com.example.ipad.unix.db.DataAccessProxy;
 import com.example.ipad.unix.db.Strings;
 import com.example.ipad.unix.model.Employee;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     int count;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int version = 1;
+        Context context = getApplication();
 
         /**create or open database*/
-        final DataAccessProxy proxy = new DataAccessProxy(this, "unix.db", 1);
+        final DataAccessProxy proxy = new DataAccessProxy(context, "unix.db", version);
 
         /** get count*/
 //        count = proxy.getCount(Employee.class, "where name=?", new String[]{"Oliver"});
@@ -67,7 +79,8 @@ public class MainActivity extends AppCompatActivity {
                         .setNeutralButton("add", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                count++;count++;
+                                count++;
+                                count++;
 
                                 /**insert data into database*/
                                 Employee employee = new Employee();
